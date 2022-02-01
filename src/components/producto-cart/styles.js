c10004480e8f1530b414436d25740f4e01338e9a
drapetti@adifse.com.ar <drapetti@adifse.com.ar>
2022-02-01 16:36:20 -0300

    Fix estilos

--- src/components/producto-cart/styles.js
+++ src/components/producto-cart/styles.js
@@ -1,33 +1,30 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions } from "react-native";
 
-const { height } = Dimensions.get('window');
+const { height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        height: height / 4,
         borderRadius: 6,
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 8,
-        elevation: 5,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20
-    },
-    header: {
+        margin: 15,
+      },
+      containerTouchable: {
+          flex: 1,
+      },
+      header: {
         flex: 1
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 15
-    },
-    details: {
-        flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    }
+      },
+      name: {
+            fontSize: 20,
+      },
+        details: {
+          flex: 1,
+          flexDirection: "row",
+          flexWrap: "wrap",
+          alignItems: 'center',
+          justifyContent: 'space-between',
+      }
 });
 
 export default styles;
